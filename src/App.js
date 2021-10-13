f747c6aab9f3c9975e1886498de53e5da550d523
mbeckdev <mbeckdev42@gmail.com>
2021-10-13 11:47:49 -0600

    Delete unnecessary component1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Component1 from './components/component1.component';
 import Header from './components/header.component';
 import Main from './components/main.component';
 import Footer from './components/footer.component';
@@ -18,7 +17,6 @@ function App() {
       <Header />
       <Main />
       <Footer />
-      <Component1 />
     </div>
   );
 }
